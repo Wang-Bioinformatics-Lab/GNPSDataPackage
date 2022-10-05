09ca89b1198ac0373b2edbc26b089f7ce3ced7cf
Mingxun Wang <mwang87@gmail.com>
2022-10-04 18:05:02 -0700

    udpating

--- src/taskresult.py
+++ src/taskresult.py
@@ -3,9 +3,14 @@ import os
 import sys
 
 
+def determine_result_url(task, result_path):
+    url = "https://proteomics2.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&file={}&block=main".format(task, result_path)
+
+    return url
+
 # This downloads a specific file from a task
 def download_task_results(task, result_path, output_file):
-    url = "https://proteomics2.ucsd.edu/ProteoSAFe/DownloadResultFile?task={}&file={}&block=main".format(task, result_path)
+    url = determine_result_url(task, result_path)
 
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
--- src/workflow_massivekb.py
+++ src/workflow_massivekb.py
@@ -4,3 +4,6 @@ import taskresult
 def download_sptxt(task, output_file):
     taskresult.download_task_results(task, "ambiguity_filtered_overlapping_protein_filtered_spectrum_library_sptxt/ambiguity_filtered_overlapping_protein_filtered_peptide_library.sptxt", output_file)
 
+def variants_dataframe(task):
+    path_to_result = "spectrum_library_tsv_splits_merged_enriched/"
+    

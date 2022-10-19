8bcdbb9248ea55c36263341ed3fe02f3479d4245
Mingxun Wang <mwang87@gmail.com>
2022-10-18 21:32:47 -0700

    adding make target

--- gnpsdata/msdata.py
+++ gnpsdata/msdata.py
@@ -10,3 +10,7 @@ def read_ms_data(input_ms_filename):
     ms1_df, ms2_df = msql_fileloading.load_data(input_ms_filename, cache=True)
 
     return ms1_df, ms2_df
+
+def read_ms_usi(input_file_usi):
+    usi = ""
+    # TODO: we need to have a resolution in the USI metabolomics resolver to point to the data by url

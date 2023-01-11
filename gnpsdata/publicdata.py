46842aa177c0a723fe7e4c28ec3849072cae5727
Mingxun Wang <mwang87@gmail.com>
2023-01-10 17:28:50 -0800

    updating

--- gnpsdata/publicdata.py
+++ gnpsdata/publicdata.py
@@ -38,3 +38,30 @@ def get_massive_private_dataset_filelist(accession, username, password):
                 files_list = files_dict["row_data"]
 
     return files_list
+
+
+def get_massive_public_dataset_filelist(accession):
+    url = "https://massive.ucsd.edu/ProteoSAFe/dataset_files.jsp?accession={}".format(accession)
+
+    # Get Data
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        print(r.text)
+        raise Exception("Unable to get dataset file list")
+
+    soup = BeautifulSoup(r.text, features="lxml")
+    scripts_list = soup.findAll('script')
+
+    files_list = []
+
+    for script in scripts_list:
+        if script.string is not None:
+            if len(script.string) > 20000:
+                print("Lets parse this one")
+                new_script = script.string.split("var dataset_files = ")[-1]
+                new_script = new_script.split("]};")[0] + "]}"
+                files_dict = json.loads(new_script)
+                files_list = files_dict["row_data"]
+
+    return files_list

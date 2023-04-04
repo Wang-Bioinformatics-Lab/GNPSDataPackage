8ac6bb041b506fb95e58ef73a838dd4e6291ff41
Mingxun Wang <mwang87@gmail.com>
2023-04-04 10:13:58 -0700

    updating fbmn output

--- gnpsdata/workflow_fbmn.py
+++ gnpsdata/workflow_fbmn.py
@@ -11,11 +11,21 @@ def get_graphml_network(task):
 
     return G
 
-def download_mgf(task, output_file):
-    taskresult.download_task_resultfile(task, "spectra_reformatted/", output_file)
-
-def download_qiime2(task, output_file):
-    taskresult.download_task_resultfile(task, "qiime2_output/qiime2_table.qza", output_file)
+def download_quantification(task, output_file):
+    taskresult.download_task_resultfile(task, "quantification_table/", output_file)
 
 def download_metadata(task, output_file):
     taskresult.download_task_resultfile(task, "metadata_merged/", output_file)
+
+def download_mgf(task, output_file):
+    taskresult.download_task_resultfile(task, "spectra_reformatted/", output_file)
+
+# Qiime2 Data
+def download_qiime2(task, output_file):
+    taskresult.download_task_resultfile(task, "qiime2_output/qiime2_table.qza", output_file)
+
+def download_qiime2_manifest(task, output_file):
+    taskresult.download_task_resultfile(task, "qiime2_output/qiime2_manifest.tsv", output_file)
+
+def download_qiime2_metadata(task, output_file):
+    taskresult.download_task_resultfile(task, "qiime2_output/qiime2_metadata.tsv", output_file)

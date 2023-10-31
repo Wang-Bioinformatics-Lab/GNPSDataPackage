d9656ad062f0ba5ffa03602b6e742eff05593194
Mingxun Wang <mwang87@gmail.com>
2023-10-30 17:32:43 -0700

    adding all the gnps2 for fbmn

--- gnpsdata/workflow_fbmn.py
+++ gnpsdata/workflow_fbmn.py
@@ -19,15 +19,21 @@ def get_graphml_network(task, gnps2=True):
 
 def download_quantification(task, output_file, gnps2=True):
     if gnps2:
-        taskresult.download_gnps2_task_resultfile(task, "nf_output/networking/quantification_table.tsv", output_file)
+        taskresult.download_gnps2_task_resultfile(task, "nf_output/clustering/featuretable_reformated.csv", output_file)
     else:
         taskresult.download_task_resultfile(task, "quantification_table/", output_file)
 
-def download_metadata(task, output_file):
-    taskresult.download_task_resultfile(task, "metadata_merged/", output_file)
+def download_metadata(task, output_file, gnps2=True):
+    if gnps2:
+        taskresult.download_gnps2_task_resultfile(task, "nf_output/metadata/merged_metadata.tsv", output_file)
+    else:
+        taskresult.download_task_resultfile(task, "metadata_merged/", output_file)
 
-def download_mgf(task, output_file):
-    taskresult.download_task_resultfile(task, "spectra_reformatted/", output_file)
+def download_mgf(task, output_file, gnps2=True):
+    if gnps2:
+        taskresult.download_gnps2_task_resultfile(task, "nf_output/clustering/spectra_reformatted.mgf", output_file)
+    else:
+        taskresult.download_task_resultfile(task, "spectra_reformatted/", output_file)
 
 # Qiime2 Data
 def download_qiime2(task, output_file):

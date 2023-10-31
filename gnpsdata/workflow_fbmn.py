6568e1d82652d86ac2b7e342a1869eec92da6285
Mingxun Wang <mwang87@gmail.com>
2023-10-30 17:27:33 -0700

    a bit more gnps2 flare

--- gnpsdata/workflow_fbmn.py
+++ gnpsdata/workflow_fbmn.py
@@ -1,18 +1,27 @@
 import networkx as nx
 from gnpsdata import taskresult
 
-def download_graphml(task, output_file):
-    taskresult.download_task_resultfile(task, "gnps_molecular_network_graphml/", output_file)
+def download_graphml(task, output_file, gnps2=True):
+    if gnps2:
+        taskresult.download_gnps2_task_resultfile(task, "nf_output/networking/network.graphml", output_file)
+    else:
+        taskresult.download_task_resultfile(task, "gnps_molecular_network_graphml/", output_file)
 
-def get_graphml_network(task):
-    taskresult.download_task_resultfile(task, "gnps_molecular_network_graphml/", "temp.graphml")
+def get_graphml_network(task, gnps2=True):
+    if gnps2:
+        taskresult.download_gnps2_task_resultfile(task, "nf_output/networking/network.graphml", "temp.graphml")
+    else:
+        taskresult.download_task_resultfile(task, "gnps_molecular_network_graphml/", "temp.graphml")
 
     G = nx.read_graphml("temp.graphml")
 
     return G
 
-def download_quantification(task, output_file):
-    taskresult.download_task_resultfile(task, "quantification_table/", output_file)
+def download_quantification(task, output_file, gnps2=True):
+    if gnps2:
+        taskresult.download_gnps2_task_resultfile(task, "nf_output/networking/quantification_table.tsv", output_file)
+    else:
+        taskresult.download_task_resultfile(task, "quantification_table/", output_file)
 
 def download_metadata(task, output_file):
     taskresult.download_task_resultfile(task, "metadata_merged/", output_file)

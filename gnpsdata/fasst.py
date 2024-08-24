392f10bce8eef3ae3f7d9175e0e4d160c6738cae
Mingxun Wang <mwang87@gmail.com>
2024-08-24 11:28:51 -0700

    adding more api changes for testing

--- gnpsdata/fasst.py
+++ gnpsdata/fasst.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import argparse
+import os
 import json
 import requests
 from tqdm import tqdm
 
-def query_fasst_usi(usi, database, host="https://fasst.gnps2.org/search",
+def query_fasst_usi(usi, database, host="https://fasst.gnps2.org",
                     analog=False, precursor_mz_tol=0.05,
                     fragment_mz_tol=0.05, min_cos=0.7,
                     cache="Yes"):
@@ -18,12 +19,12 @@ def query_fasst_usi(usi, database, host="https://fasst.gnps2.org/search",
         "cache": cache
     }
 
-    r = requests.get(host, params=params, timeout=50)
+    r = requests.get(os.path.join(host, "search"), params=params, timeout=50)
     r.raise_for_status()
 
     return r.json()
 
-def query_fasst_peaks(precursor_mz, peaks, database, serverurl="https://fasst.gnps2.org/", analog=False, precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7):
+def query_fasst_peaks(precursor_mz, peaks, database, host="https://fasst.gnps2.org", analog=False, precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7):
     spectrum_query = {
         "peaks": peaks,
         "precursor_mz": precursor_mz
@@ -38,8 +39,14 @@ def query_fasst_peaks(precursor_mz, peaks, database, serverurl="https://fasst.gn
         "cosine_threshold": min_cos,
     }
 
-    r = requests.post(serverurl + "search", data=params, timeout=50)
+    r = requests.post(os.path.join(host, "search"), data=params, timeout=50)
 
     r.raise_for_status()
 
     return r.json()
+
+
+def get_databases(host="https://fasst.gnps2.org"):
+    url = "{}/libraries".format(host)
+
+    return requests.get(url).json()
--- test/test_fasst.py
+++ test/test_fasst.py
@@ -1,6 +1,10 @@
 import sys
+import requests
 sys.path.insert(0, '..')
 sys.path.insert(0, '../modules/GNPSDataPackage/')
+
+FASST_SERVER_URL = "https://fasst.gnps2.org"
+#FASST_SERVER_URL = "http://169.235.26.140:5054"
 
 def test_fasst_usi_search():
     print("USI Search")
@@ -9,10 +13,7 @@ def test_fasst_usi_search():
 
     usi = "mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00005435899"
 
-    host="https://fasst.gnps2.org/search"
-    #host="http://169.235.26.140:5054/search"
-
-    results = fasst.query_fasst_usi(usi, "gnpsdata_index_11_25_23", host=host, analog=False, \
+    results = fasst.query_fasst_usi(usi, "gnpsdata_index_11_25_23", host=FASST_SERVER_URL, analog=False, \
                     precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, cache="No")
 
 
@@ -20,8 +21,38 @@ def test_fasst_usi_search():
 
     # TODO: Testing the results to resolve the MS/MS on them
 
+
+    print(results["results"][0])
+
+def test_fasst_usi_search_mtbls():
+    print("USI Search")
+
+    from gnpsdata import fasst
+
+    usi = "mzspec:MSV000083540:ccms_peak/Pooled_Urine_5_posneg_b.mzML:scan:5443"
+
+    results = fasst.query_fasst_usi(usi, "mtbls_7_23_24", host=FASST_SERVER_URL, analog=False, \
+                    precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, cache="No")
+
+
+    assert(len(results["results"]) > 50)
+
+
+def test_libraries_list():
+    url = "{}/libraries".format(FASST_SERVER_URL)
+
+    from gnpsdata import fasst
+    
+    libraries = fasst.get_databases(host=FASST_SERVER_URL)
+
+    print(libraries)
+
+    assert(len(libraries) > 0)
+
 def main():
-    test_fasst_usi_search()
+    #test_fasst_usi_search()
+    test_fasst_usi_search_mtbls()
+    #test_libraries_list()
 
 if __name__ == "__main__":
     main()

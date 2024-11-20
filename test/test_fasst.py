445d82041ac6dc9215e7e2726c7fb5d2d7254c65
Mingxun Wang <mwang87@gmail.com>
2024-11-20 10:22:34 -0800

    updating the testing

--- test/test_fasst.py
+++ test/test_fasst.py
@@ -13,7 +13,16 @@ def test_fasst_usi_search():
 
     usi = "mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00005435899"
 
-    results = fasst.query_fasst_usi(usi, "gnpsdata_index_11_25_23", host=FASST_SERVER_URL, analog=False, \
+    results = fasst.query_fasst_usi(usi, "metabolomicspanrepo_index_nightly", host=FASST_SERVER_URL, analog=False, \
+                    precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, cache="No")
+
+
+    assert(len(results["results"]) > 50)
+
+
+    usi = "mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00005435899"
+
+    results = fasst.query_fasst_usi(usi, "metabolomicspanrepo_index_latest", host=FASST_SERVER_URL, analog=False, \
                     precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, cache="No")
 
 
@@ -23,20 +32,6 @@ def test_fasst_usi_search():
 
     print(results["results"][0])
 
-def test_fasst_usi_search_mtbls():
-    print("USI Search")
-
-    from gnpsdata import fasst
-
-    usi = "mzspec:MSV000083540:ccms_peak/Pooled_Urine_5_posneg_b.mzML:scan:5443"
-
-    results = fasst.query_fasst_usi(usi, "mtbls_7_23_24", host=FASST_SERVER_URL, analog=False, \
-                    precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, cache="No")
-
-
-    assert(len(results["results"]) > 50)
-
-    print(results["results"][0])
 
 
 def test_libraries_list():

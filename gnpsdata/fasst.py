e9049c293cd62237812f10a5c265a9679f27aa2f
Mingxun Wang <mwang87@gmail.com>
2025-07-30 10:33:46 -0700

    adding wip code

--- gnpsdata/fasst.py
+++ gnpsdata/fasst.py
@@ -3,8 +3,10 @@ import argparse
 import os
 import json
 import requests
+import time
 from tqdm import tqdm
 
+# this is sychronous low performance
 def query_fasst_usi(usi, database, host="https://fasst.gnps2.org",
                     analog=False, precursor_mz_tol=0.05,
                     fragment_mz_tol=0.05, min_cos=0.7,
@@ -21,6 +23,43 @@ def query_fasst_usi(usi, database, host="https://fasst.gnps2.org",
 
     r = requests.get(os.path.join(host, "search"), params=params, timeout=50)
     r.raise_for_status()
+
+    return r.json()
+
+# high performance version
+def query_fasst_api_usi(usi, database, host="https://api.fasst.gnps2.org",
+                    analog=False, precursor_mz_tol=0.05,
+                    fragment_mz_tol=0.05, min_cos=0.7,
+                    cache="Yes"):
+    params = {
+        "usi": usi,
+        "library": database,
+        "analog": "Yes" if analog else "No",
+        "pm_tolerance": precursor_mz_tol,
+        "fragment_tolerance": fragment_mz_tol,
+        "cosine_threshold": min_cos,
+        "cache": cache
+    }
+
+    print(os.path.join(host, "search"))
+
+    r = requests.get(os.path.join(host, "search"), params=params, timeout=50)
+
+    r.raise_for_status()
+
+    print(r.json())
+
+    task_id = r.json()["id"]
+    
+    # now we will wait and then get the results
+    while True:
+        time.sleep(1)
+        r = requests.post(os.path.join(host, "search/result/{}".format(task_id)), timeout=50)
+
+        r.raise_for_status()
+
+        return r.json()
+
 
     return r.json()
 
--- test/test_fasst.py
+++ test/test_fasst.py
@@ -4,6 +4,7 @@ sys.path.insert(0, '..')
 sys.path.insert(0, '../modules/GNPSDataPackage/')
 
 FASST_SERVER_URL = "https://fasst.gnps2.org"
+FASST_API_SERVER_URL = "https://api.fasst.gnps2.org"
 #FASST_SERVER_URL = "http://169.235.26.140:5054"
 
 def test_fasst_usi_search():
@@ -32,7 +33,24 @@ def test_fasst_usi_search():
 
     print(results["results"][0])
 
+def test_fasst_api_usi_search():
+    print("USI Search API")
 
+    from gnpsdata import fasst
+
+    usi = "mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00005435899"
+
+    results = fasst.query_fasst_api_usi(usi, "metabolomicspanrepo_index_nightly", host=FASST_API_SERVER_URL, analog=False, \
+                    precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, cache="No")
+
+    assert(len(results["results"]) > 50)
+
+    usi = "mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00005435899"
+
+    results = fasst.query_fasst_api_usi(usi, "metabolomicspanrepo_index_latest", host=FASST_API_SERVER_URL, analog=False, \
+                    precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, cache="No")
+
+    assert(len(results["results"]) > 50)
 
 def test_libraries_list():
     url = "{}/libraries".format(FASST_SERVER_URL)
@@ -47,7 +65,7 @@ def test_libraries_list():
 
 def main():
     #test_fasst_usi_search()
-    test_fasst_usi_search_mtbls()
+    test_fasst_api_usi_search()
     #test_libraries_list()
 
 if __name__ == "__main__":

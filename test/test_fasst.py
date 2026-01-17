46779f30733553aeffd826782abbbe46bc960498
Mingxun Wang <mwang87@gmail.com>
2026-01-17 13:28:21 -0800

    testing streamline

--- test/test_fasst.py
+++ test/test_fasst.py
@@ -104,7 +104,7 @@ def test_throughput_api_search():
     for i in range(0, 20):
         print("submitted", i)
         results = fasst.query_fasst_api_usi(usi, "metabolomicspanrepo_index_nightly", host=FASST_API_SERVER_URL, analog=False, \
-                    precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, cache="Yes", blocking=False)
+                    precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7, blocking=False)
         
         status_results_list.append(results)
 
@@ -186,6 +186,110 @@ def test_yasin_incomplete_api_search():
     assert (len(results["results"]) > 7000)
 
 
+def test_yasin_analog_api_search():
+    usi = "mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00016217549"
+
+    from gnpsdata import fasst
+
+    results = fasst.query_fasst_api_usi(usi, "metabolomicspanrepo_index_nightly", host=FASST_API_SERVER_URL, analog=True, \
+                    precursor_mz_tol=0.1, fragment_mz_tol=0.1, min_cos=0.7, lower_delta=70,
+                    upper_delta=70, blocking=True)
+    
+    print("Total Analog Hits", len(results["results"]))
+
+def debug_yasin_large_analog_api_search():
+    from gnpsdata import fasst
+    import pandas as pd
+
+    usi_df = pd.read_csv("data/test_usis.tsv")  
+
+    print("Total USIs to test:", len(usi_df))
+
+    status_results_list = []
+    for index, row in usi_df.iterrows():
+        usi = row["usi"]
+
+        print("Testing USI:", usi)
+
+        results = fasst.query_fasst_api_usi(usi, "metabolomicspanrepo_index_nightly", host=FASST_API_SERVER_URL, analog=True, \
+                        precursor_mz_tol=0.1, fragment_mz_tol=0.1, min_cos=0.7, lower_delta=70,
+                        upper_delta=70, blocking=False)
+        
+        status_results_list.append([results, usi])
+    
+    # now lets wait and get the results
+    # lets now wait for all the results to be ready
+    time.sleep(1)
+    for k in range(0, 180):
+        all_results_finished = True
+        count_finished = 0
+        for i, result_entry in enumerate(status_results_list):
+            status = result_entry[0]
+            usi = result_entry[1]
+            if status["status"] == "DONE" or status["status"] == "TIMEOUT":
+                count_finished += 1
+                continue
+            
+            # checking on the results
+            all_results_finished = False
+            try:
+                results = fasst.get_results(status, blocking=False)
+                if results == "PENDING":
+                    status["status"] = "PENDING"
+                    print("Pending Results for", i)
+                    continue
+                else:
+                    status["status"] = "DONE"
+                    status["results"] = results["results"]
+            except:
+                status["status"] = "ERROR"
+                print("Error Results for", usi)
+                continue
+
+        if all_results_finished:
+            break
+        print("Finished so far:", count_finished, "out of", len(status_results_list))
+        time.sleep(5)
+
+    # Initialize a dictionary to hold the counts
+    counts = {
+        "DONE": 0, 
+        "PENDING": 0, 
+        "ERROR": 0, 
+        "UNKNOWN": 0
+    }
+
+    # Summarizing the results
+    for i, result_entry in enumerate(status_results_list):
+        status = result_entry[0]
+        usi = result_entry[1]
+
+        current_status = status["status"]
+        
+        if current_status == "DONE":
+            counts["DONE"] += 1
+            print("Total Hits", len(status["results"]))
+        elif current_status == "PENDING":
+            counts["PENDING"] += 1
+            print("Pending Results")
+        elif current_status == "ERROR":
+            counts["ERROR"] += 1
+            print("Error in Results")
+        else:
+            counts["UNKNOWN"] += 1
+            print("Unknown Status", current_status)
+
+    # Print the final tally
+    print("-" * 20)
+    print("Final Counts:")
+    print(f"DONE:    {counts['DONE']}")
+    print(f"PENDING: {counts['PENDING']}")
+    print(f"ERROR:   {counts['ERROR']}")
+    print(f"UNKNOWN: {counts['UNKNOWN']}")
+    
+        
+
+
 def test_libraries_list():
     url = "{}/libraries".format(FASST_SERVER_URL)
 
@@ -205,7 +309,7 @@ def debug_local():
     results = fasst.query_fasst_api_usi(usi, "gnpslibrary", host="http://localhost:5055", analog=False, \
                     precursor_mz_tol=0.05, fragment_mz_tol=0.05, min_cos=0.7)
 
-    
+    print(results)
 
 
 def main():
@@ -216,10 +320,12 @@ def main():
     #test_throughput_api_search()
     #test_big_analog_api_search()
     #test_yasin_incomplete_api_search()
+    #test_yasin_analog_api_search()
+    debug_yasin_large_analog_api_search()
     
     #test_libraries_list()
 
-    debug_local()
+    #debug_local()
 
 if __name__ == "__main__":
     main()
